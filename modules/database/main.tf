1b2d00d5f38ccb6c9d18897bf40939ea6a464bd8
venkata-suresh-bukka <155045637+venkata-suresh-bukka@users.noreply.github.com>
2025-03-27 20:16:07 +0530

    added backend

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -38,7 +38,7 @@ resource "aws_db_instance" "rds" {
 }
 
 resource "aws_secretsmanager_secret" "db_secret" {
-  name = "wordpress-db-secret9"
+  name = "wordpress-db-secret10"
 }
 
 resource "aws_secretsmanager_secret_version" "db_secret_version" {
--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+
+terraform {
+  backend "s3" {
+    bucket         = "store-statefiles"         # Replace with your actual S3 bucket name
+    key            = "terraform.tfstate"        # Path to the state file in S3
+    region         = "us-east-1"                # AWS Region where S3 is hosted
+    encrypt        = true                       # Enables server-side encryption
+    # dynamodb_table = "terraform-locks"         # (Optional) DynamoDB for state locking
+  }
+}
 provider "aws" {
   region = var.aws_region
 }
