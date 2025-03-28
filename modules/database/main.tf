9ef011fca0814404ca3b222ee2740d48dcdc4b47
venkata-suresh-bukka <155045637+venkata-suresh-bukka@users.noreply.github.com>
2025-03-28 16:38:12 +0530

    Update main.tf

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -38,7 +38,7 @@ resource "aws_db_instance" "rds" {
 }
 
 resource "aws_secretsmanager_secret" "db_secret" {
-  name = "wordpress-db-secret11"
+  name = "wordpress-db-secret12"
 }
 
 resource "aws_secretsmanager_secret_version" "db_secret_version" {
